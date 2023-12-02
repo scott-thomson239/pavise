abab55722bf33f5a967bc6ffc7e567e7d8621658
Scott Thomson <110630636+scott-thomson239@users.noreply.github.com>
2023-12-02 14:08:53 +0000

    remove template licenses

--- core/src/main/scala/pavise/Serializer.scala
+++ core/src/main/scala/pavise/Serializer.scala
@@ -1,16 +1,10 @@
 package pavise
 
 import scodec.bits.ByteVector
-import pavise.Serializer.KeyOrValue
-import pavise.Serializer.*
 
-trait GenericSerializer[-T <: KeyOrValue, F[_], A]:
+sealed trait GenericSerializer[F[_], A]:
   def serialize(topic: String, headers: List[Header], a: A): F[ByteVector]
 
-type KeySerializer[F[_], A] = GenericSerializer[Key, F, A]
-type ValueSerializer[F[_], A] = GenericSerializer[Value, F, A]
+trait KeySerializer[F[_], A] extends GenericSerializer[F, A]
+trait ValueSerializer[F[_], A] extends GenericSerializer[F, A]
 
-object Serializer:
-  sealed trait KeyOrValue
-  sealed trait Key extends KeyOrValue
-  sealed trait Value extends KeyOrValue
